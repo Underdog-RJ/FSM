915324abb879da9969f0c320e60dbb07a521e655
RJ <1768976999@qq.com>
2023-02-27 11:56:23 +0800

    update

--- genMp4.py
+++ genMp4.py
@@ -14,9 +14,9 @@ def run():
             mkdirStr = "sudo mkdir -p {}".format(dir_path)
             os.system(mkdirStr)
             os.system("cd {}".format(dir_path))
-            str = "esmini --window 60 60 800 400 --osc /home/Rupeng119_com/esmini/resources/xosc/{}.xosc --fixed_timestep 0.033 --capture_screen".format(
+            esminiCmd = "esmini --window 60 60 800 400 --osc /home/Rupeng119_com/esmini/resources/xosc/{}.xosc --fixed_timestep 0.033 --capture_screen".format(
                 i)
-            os.system(str)
+            os.system(esminiCmd)
             mp4Cmd = "ffmpeg -f image2 -framerate 30 -i screen_shot_%5d.tga -c:v libx264 -qp 0 out.mp4"
             os.system(mp4Cmd)
 
